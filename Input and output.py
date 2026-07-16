fbd11fe144129df5c38f45729aa42bdc613646ad
Misbah Uddin <163721297+Akhil112438@users.noreply.github.com>
2026-07-16 18:40:35 +0500

    Add files via upload

---
+++ Input and output.py
@@ -0,0 +1,13 @@
+# #output
+# name ="misbah"
+# age = 20
+# print("my name is",name,"and my age is",age)
+
+# #fornmatted string
+# name ="misbah"
+# age = 20
+# print(f"my name is {name} and my age is {age}")
+
+# #input
+age = int(input("what is your age? "))
+print(age)
