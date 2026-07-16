ebd2d0f887b93e8825f9ec5689169d4a12acfec8
Misbah Uddin <163721297+Akhil112438@users.noreply.github.com>
2026-07-16 18:39:58 +0500

    Add files via upload

---
+++ comparison operators.py
@@ -0,0 +1,30 @@
+# a = 12
+# b = 12
+# print(a == b)  # True, because both values are equal
+# print(a != b)  # False, because both values are equal
+# print(a < b)   # False, because both values are equal
+# print(a > b)   # False, because both values are equal
+# print(a <= b)  # True, because both values are equal
+# print(a >= b)  # True, because both values are equal
+
+# a = 20.1
+# b = 20
+# print(a == b)  # False, because the values are not equal
+# print(a != b)  # True, because the values are not equal
+# print(a < b)   # False, because 20.1 is greater than 20\
+# print(a > b)   # True, because 20.1 is greater than 20
+# print(a <= b)  # False, because 20.1 is greater than 20
+# print(a >= b)  # True, because 20.1 is greater than 20
+
+# print(25 < 47) # True, because 25 is less than 47
+
+# print(15 > 15) # False, because both values are equal
+# print(15 >= 15) # True, because both values are equal
+# print(13 <= 13) # True, because both values are equal
+
+
+# print(ord("a"))  # 97, because the Unicode code point for 'a' is 97
+# print(ord("A"))  # 65, because the Unicode code point for 'A' is 65
+# print("ABC" > "ABD")  # False, because 'C' is less than 'D'
+
+# print("ABC" < 123)  # TypeError: '<' not supported between instances of 'str' and 'int'
