a35611a117339a9ba834c95ce5179e007890429f
Misbah Uddin <163721297+Akhil112438@users.noreply.github.com>
2026-07-16 18:41:02 +0500

    Add files via upload

---
+++ Operators and Arthimetic Operators.py
@@ -0,0 +1,15 @@
+#Arthimetic operators
+
+ a = 20
+ b = 15
+ print(a + b) #Addition
+ print(a - b) #Subtraction   
+ print(a * b) #Multiplication
+ print(a / b) #Division
+ print(a % b) #Modulus
+ print(a ** b) #Exponentiation
+ print(a // b) #Floor division
+ print(20+15/2) #BODMAS rule
+
+
+
