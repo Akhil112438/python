97a40aad72914418c38b2962b8687010e67054b8
Misbah Uddin <163721297+Akhil112438@users.noreply.github.com>
2026-07-20 19:57:26 +0500

    Add files via upload

---
+++ if else and conditional statement.py
@@ -0,0 +1,53 @@
+#if else
+
+# a = 13
+
+# if a > 10:
+#     print("a is greater then 10")
+
+# else:
+#     print("a is less then 10")
+
+
+# money = int(input("please provide me money :-"))
+
+# if money == 10:
+#     print(" i will have the ice-cream")
+
+# elif money == 20:
+#     print("i will have the chocolate")
+
+# elif money == 30:
+#     print("i will have the cake")
+    
+# else:
+#     print("i will have the water")
+
+
+
+
+# num1 = int(input(" tell me first number"))
+# num2 = int(input(" tell me second number"))
+
+# if num1 > num2:
+#     print(f"{num1} is greater then {num2}")
+
+# elif num2 > num1:
+#     print(f"{num2} is greater then {num1}")
+
+# else:
+#     print(" Both the number sare same")
+
+
+
+
+gen = input("please provide me your gender as character(M or F):-")
+
+if gen == "M":
+    print("Good Moring Sir")
+
+elif gen == "F":
+    print("Good Moring Ma'am")
+
+else:
+    print("Good Moring")
