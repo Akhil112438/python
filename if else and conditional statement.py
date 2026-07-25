f972a085546e162ff0599215e0bc39848c7b2ab2
Misbah Uddin <163721297+Akhil112438@users.noreply.github.com>
2026-07-25 12:30:15 +0500

    Add files via upload

--- if else and conditional statement.py
+++ if else and conditional statement.py
@@ -41,13 +41,69 @@
 
 
 
-gen = input("please provide me your gender as character(M or F):-")
+# gen = input("please provide me your gender as character(M or F):-")
 
-if gen == "M":
-    print("Good Moring Sir")
+# if gen == "M" or gen == "m":
+#     print("Good Moring Sir")
 
-elif gen == "F":
-    print("Good Moring Ma'am")
+# elif gen == "F" or gen == "f":
+#     print("Good Moring Ma'am")
+
+# else:
+#     print("unknown gender")
+
+# num = int(input("please provide me number:-"))
+
+# if num % 2 == 0:
+#     print(" even number")
+# else:
+#     print(" odd number")    
+
+
+# num = input("please provide your name :-") 
+# age = int(input("now provide your age :-"))
+
+# if age >= 18:
+#     print(f"Hello {num} you are a valid voter")
+# else:
+#     print(f"Hello {num} you are not a valid voter")
+
+
+#leap year
+# year = int(input("tell your year:-"))
+# if year % 100 == 0 and year % 400 == 0:
+#     print("its a leap year")
+
+
+# elif year %100 != 0 and year % 4 == 0:
+#     print("its a leap year")
+
+# else:
+#     print("its a normal year")
+
+
+#if- elif ladder
+temp=int(input("tell me the temperature:- "))
+
+if temp < 0:
+    print("freezing weather")
+
+elif temp >= 0 and temp < 10:
+    print("very cold weather")
+
+
+elif temp >= 10 and temp < 20:
+    print("cold weather")
+
+
+elif temp >= 20 and temp < 30:
+    print("normal weather")
+
+elif temp >= 30 and temp < 40:
+    print("hot weather")
 
 else:
-    print("Good Moring")
+    print("very hot weather")
+
+
+
